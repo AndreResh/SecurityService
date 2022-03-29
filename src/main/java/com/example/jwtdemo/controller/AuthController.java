06f82640dad82214beb4d981545acdd0e99c8262
AndreResh <reshetnyak.andrej2016@yandex.ru>
2022-03-29 13:48:28 +0300

    add logger to controller

--- src/main/java/com/example/jwtdemo/controller/AuthController.java
+++ src/main/java/com/example/jwtdemo/controller/AuthController.java
@@ -3,10 +3,11 @@ package com.example.jwtdemo.controller;
 import com.example.jwtdemo.pojo.LoginRequest;
 import com.example.jwtdemo.pojo.SignupRequest;
 import com.example.jwtdemo.service.UsersService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/authenticate")
 public class AuthController {
@@ -19,11 +20,13 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        log.info("Login user: {}",loginRequest);
         return ResponseEntity.ok(usersService.login(loginRequest));
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody SignupRequest signupRequest) {
+        log.info("Register user: {}",signupRequest);
         return ResponseEntity.ok(usersService.register(signupRequest));
     }
 }
